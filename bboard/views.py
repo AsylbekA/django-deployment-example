4f62217ba584d8f8596db87442a696b2f191dc3b
Asylbeka <anarbayasylbek@gmail.com>
2019-10-22 12:24:42 +0600

    added to index cont and rubr

--- bboard/views.py
+++ bboard/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
 
 
 def by_rubric(request, rubric_id):
