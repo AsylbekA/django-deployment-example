72631e38675ca6c7a0f390c0da666f6d6d31ac39
Asylbeka <anarbayasylbek@gmail.com>
2019-10-22 09:54:46 +0600

    '001-initial'

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse as hr
+from django.http import HttpResponse
+from .models import Bb
+
 
 def index(request):
-    return hr("Здесь будет выведен список обьявлений.")
-# Create your views here.
+    s = 'Список обьявлений \r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content + '\r\r\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8 ')
