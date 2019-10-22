b79783924273a391fd849f69a1fdf4bcc7ec6133
Asylbeka <anarbayasylbek@gmail.com>
2019-10-22 10:50:08 +0600

    Функция сокращение

--- bboard/views.py
+++ bboard/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Bb
-from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(request, 'bboard/index.html', {'bbs': bbs})
