2401864938f93228c711c1e492313a0d8186b1e2
Asylbeka <anarbayasylbek@gmail.com>
2019-10-22 12:00:46 +0600

    create by_rubric.html

--- bboard/views.py
+++ bboard/views.py
@@ -1,10 +1,15 @@
+from .models import Bb, Rubric
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Bb
 
 
 def index(request):
-    s = 'Список обьявлений \r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\r\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8 ')
+    bbs = Bb.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
