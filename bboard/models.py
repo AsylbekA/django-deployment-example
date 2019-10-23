75f586a455a33f380cf50fd7e158b02119dc6457
Asylbeka <anarbayasylbek@gmail.com>
2019-10-23 21:51:27 +0600

    4.4пример валидатора-класса

--- bboard/models.py
+++ bboard/models.py
@@ -74,3 +74,17 @@ def validate_even(val):
     if val % 2 != 0:
         raise ValidationError('Число % (value) s нечетнoе', code='odd',
                               params={'value': val})
+
+
+class MinMaxValueValidator:
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __call__(self, val):
+        if val < self.min_value or val > self.max_value:
+            raise ValidationError('Введенное число должно ' +\
+                                  ' находиться в диапозоне от % (min)s до %(max)s',
+                                  code='out_of _range',
+                                  params={'min': self.min_value, 'max': self.max_value})
+
