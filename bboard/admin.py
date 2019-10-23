67b98b669714d802a906dd68a6a2a5dcdffd355d
Asylbeka <anarbayasylbek@gmail.com>
2019-10-23 21:52:27 +0600

    update migrate

--- bboard/admin.py
+++ bboard/admin.py
@@ -3,7 +3,7 @@ from .models import Bb, Rubric
 
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'rubric', 'published')
+    list_display = ('title', 'kind', 'content', 'price', 'rubric', 'published')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
 
