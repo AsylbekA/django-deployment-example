62be8a7d5dbe4c8794401832c31f71de1d4da4a3
Asylbeka <anarbayasylbek@gmail.com>
2019-10-24 13:06:52 +0600

    Обьявим маршруты, ведущие на эти контроллеры

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,8 +1,10 @@
-from django.urls import re_path
-from .views import index, by_rubric, BbCreateView
+from django.urls import re_path, path
+from .views import index, by_rubric, BbCreateView, add, add_save
 
 
 urlpatterns = [
+    path('add/save/', add_save, name='add'),
+    path('add/', add, name='add'),
     re_path(r'^add/$', BbCreateView.as_view(), name='add'),
     re_path(r'^$', index, name='index'),
     re_path(r'^(?P<rubric_id>[0-9]*)/$', by_rubric, name='by_rubric'),
